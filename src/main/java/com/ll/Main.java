c831b707ddc05441f5c7addea17a6144a174ce50
choigpt <choigptroadbalancer@gmail.com>
2026-07-03 21:37:53 +0900

    Implement basic Spring framework IoC context

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,6 +1,9 @@
 package com.ll;
 
+import com.ll.framework.ioc.ApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+
     }
 }
--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,27 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+
+public class ApplicationContext {
+    private final TestPostRepository testPostRepository;
+    private final TestPostService testPostService;
+    private final TestFacadePostService testFacadePostService;
+
+    public ApplicationContext() {
+        testPostRepository = new TestPostRepository();
+        testPostService = new TestPostService(testPostRepository);
+        testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
     }
 
-    public <T> T genBean(String beanName) {
-        return (T) null;
+    public <T> T genBean(String name) {
+        return switch (name) {
+            case "testPostRepository" -> (T) testPostRepository;
+            case "testPostService" -> (T) testPostService;
+            case "testFacadePostService" -> (T) testFacadePostService;
+            default -> null;
+        };
     }
 }
